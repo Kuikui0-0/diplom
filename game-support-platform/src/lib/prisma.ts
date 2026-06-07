4996b12e18cc11a9108ad8d773bb651cc697edb7
Денис <117908090+Denchik0-0@users.noreply.github.com>
2026-06-07 14:02:16 +0000

    fix: ss

--- game-support-platform/src/lib/prisma.ts
+++ game-support-platform/src/lib/prisma.ts
@@ -1,15 +1,3 @@
 import { PrismaClient } from '@prisma/client';
-import { PrismaBetterSqlite3 } from '@prisma/adapter-better-sqlite3';
-
-const adapter = new PrismaBetterSqlite3({
-  url: process.env.DATABASE_URL || "file:./prisma/dev.db",
-});
-
-const prismaClientSingleton = () => {
-  return new PrismaClient({ adapter });
-};
-
-const prisma = globalThis.prismaGlobal ?? prismaClientSingleton();
+const prisma = new PrismaClient();
 export default prisma;
-
-if (process.env.NODE_ENV !== 'production') globalThis.prismaGlobal = prisma;
