a892ab9487ff48a9afc868788f27e3ebf5bdb41a
Денис <117908090+Denchik0-0@users.noreply.github.com>
2026-06-09 00:09:54 +0000

    fff

--- game-support-platform/prisma.config.ts
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'prisma/config'
-
-export default defineConfig({
-  datasource: {
-      url: "postgres://09913cdec468848ef7dd98727af650e81e6cec13c251636116d088d446c0cc8b:sk_arMOPIc7tmsBQ38YFnE5T@db.prisma.io:5432/postgres?sslmode=require"
-  },
-})
